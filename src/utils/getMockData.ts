9d258c52df69fe8abd41084d56b5525c02f100c5
daveyhert <daveyhert@gmail.com>
2025-06-22 14:03:03 +0100

    refactor: Update mockProfiles and add getMockData utility

---
+++ src/utils/getMockData.ts
@@ -0,0 +1,6 @@
+import { mockApplications } from "../data/mockApplications";
+import { mockProfiles, defaultUserProfile } from "../data/mockProfiles";
+
+export function getMockData() {
+  return { mockApplications, mockProfiles, defaultUserProfile };
+}
